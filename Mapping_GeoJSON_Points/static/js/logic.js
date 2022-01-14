1fe2f652b54229e76432e92cb4aad4cd98d4fb74
Cathleen Mai <91682586+Julcatan@users.noreply.github.com>
2022-01-14 14:29:56 -0500

    commit after adding majorAirport.json

---
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -0,0 +1,120 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+
+// Create the map object with center at the San Francisco airport.
+//let map = L.map('mapid').setView([30.18999924, -97.668663992], 5);
+// Create the map object with center at the San Francisco airport.
+let map = L.map('mapid').setView([37.5, -122.5], 10);
+//let map = L.map('mapid').setView([36.1733, -120.1794],7);
+// Create the map object with a center and zoom level.
+//old center setView([40.7, -94.5], 4);
+
+// Coordinates for each point to be used in the line.
+// Coordinates for each point to be used in the polyline.
+//let line = [
+   // [33.9416, -118.4085], //LAX
+    //[37.6213, -122.3790], //SFO
+   // [40.7899, -111.9791], //SLC
+    //[47.4502, -122.3088],// SEA
+   // [30.18999924, -97.668663992] ,//Austin Berg
+    //[43.67771760000001, -79.62481969999999], // Toronto
+   // [40.641766, -73.780968], // JFK
+    //[27.390665104, -82.552664456] //SRQ
+  //];
+
+// Add GeoJSON data.
+let sanFranAirport =
+{"type":"FeatureCollection","features":[{
+    "type":"Feature",
+    "properties":{
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city":"San Francisco",
+        "country":"United States",
+        "faa":"SFO",
+        "icao":"KSFO",
+        "alt":"13",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+        "geometry":{
+            "type":"Point",
+            "coordinates":[-122.375,37.61899948120117]}}
+]};
+
+
+// Grabbing our GeoJSON data.
+L.geoJSON(sanFranAirport).addTo(map);
+
+  // Create a polyline using the line coordinates and make the line red.
+//L.polyline(line, {
+    //color: "blue"
+    //,stroke-dasharray= ""
+ // }).addTo(map);
+//  Add a marker to the map for Los Angeles, California.
+//let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+//L.circle([34.0522, -118.2437], {
+  //  radius: 300,
+    //fillColor: '#FFD326',
+    //color:'black'
+ //}).addTo(map);
+
+ // alternative marker in pixel
+//L.circleMarker([34.0522, -118.2437],
+//{   radius:300,
+   // color: "black",
+   // fillcolor: "#fffa1"
+
+//}).addTo(map);
+
+
+// Grabbing our GeoJSON data.
+//L.geoJSON(sanFranAirport, {
+    // We turn each feature into a marker on the map.
+  //  pointToLayer: function(feature, latlng) {
+    //  console.log(feature);
+      //return L.marker(latlng).bindPopup("<h2>" + feature.properties.city + ", " + feature.properties.name + ", " + feature.properties.country + "</h2>");
+//    }
+
+  //}).addTo(map);
+
+
+L.geoJSON(sanFranAirport, {
+    // We turn each feature into a marker on the map.
+      onEachFeature: function(feature, layer) {
+      console.log(layer);
+    layer.bindPopup("<h2>" + feature.properties.city + feature.properties.name +feature.properties.faa);
+    }
+
+    }).addTo(map);
+  
+
+
+
+
+// Get data from cities.js
+let cityData = cities;
+
+// Loop through the cities array and create one marker for each city.
+//cityData.forEach(function(city) {
+  //  console.log(city)
+   // L.circleMarker(city.location,{radius: (city.population/100000), fillColor: "#CB8427", color: "#98652E",weight: "4"})
+    //.bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+    //.addTo(map);
+//});
+
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v11/tiles/{z}/{x}/{y}?access_token={accessToken}'
+//let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}'
+, {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: API_KEY
+});
+// Then we add our 'graymap' tile layer to the map.
+streets.addTo(map);
